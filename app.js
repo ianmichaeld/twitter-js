640389be41dcfa2691580f6dd0cbd671177ca2ae
Ian Dewsbury <ianmichaeld@gmail.com>
2018-03-06 10:08:27 -0500

    added chalk

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require( 'express' );
+const chalk = require( 'chalk' );
+const volleyball = require( 'volleyball' );
 const app = express(); // creates an instance of an express application
 
-app.use( (req, res, next) => {
-  console.log( req.method, req.path, res.statusCode )
-  next()
-})
+const port = 3000;
+
+// app.use( (req, res, next) => {
+//   console.log( req.method, req.path, res.statusCode );
+//   next();
+// })
+app.use(volleyball);
 
 app.get('/', ( req, res, next) => {
-  res.send('You are here!')
+  res.send('You are here!');
 })
 
 app.get('/news', ( req, res, next) => {
-  res.send('You want news?')
+  res.send('You want news?');
 })
 
 
-app.listen(3000)
-console.log('Server running')
+app.listen(3000);
+console.log(`Server running on port: ${port}`);
